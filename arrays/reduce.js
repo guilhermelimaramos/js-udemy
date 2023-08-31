39e97a27d209f93788000e8021af4cc9647c9454
Guilherme Lima <glimaramos013@gmail.com>
2023-08-31 10:42:40 -0300

    reduce.js modified

--- arrays/reduce.js
+++ arrays/reduce.js
@@ -1,4 +1,4 @@
-const numbers = [12, 45, 7, 2, 74, 65, 3, 15, 21]
+const numbers = [11, 42, 7, 2, 74, 65, 3, 15, 21]
 
 const total = numbers.reduce((acc, value) => acc + value, 0)
 
@@ -6,7 +6,40 @@ console.log(total) // 244
 
 console.log('------------------------------')
 
-const isPair = numbers.filter(value => value % 2 == 0)
-console.log(isPair)
+const addOnlyPair = numbers.reduce((acc, value) => {
+  if (value % 2 == 0) acc += value;
+  return acc
+}, 0)
+console.log(addOnlyPair)
 
+console.log('-----------------------------')
+
+const double = numbers.reduce((acc, value) => {
+  acc.push(value * 2);
+  return acc
+}, [])
+console.log(double)
+
+console.log('---------------------------------')
+
+console.log('---------------------------------')
+
+console.log('---------------------------------')
+
+
+const people = [
+  {name: 'Guilherme', age: 27},
+  {name: 'Mavi', age: 20},
+  {name: 'Vitória', age: 25},
+  {name: 'Luiz', age: 42},
+  {name: 'Ana', age: 32}
+];
+
+const older = people.reduce((acc, value) => {
+  if (acc.age > value.age) return acc;
+  return value
+  
+},0)
+
+console.log(older)
 
